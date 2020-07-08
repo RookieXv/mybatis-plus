d5eed36ab43decc9d5b3b501e78fa93ed034bbce
liuwenxu <liuwx8102@163.com>
2020-07-08 14:40:25 +0800

    完善

--- src/main/java/com/example/mybatisplus/MybatisPlusApplication.java
+++ src/main/java/com/example/mybatisplus/MybatisPlusApplication.java
@@ -3,9 +3,11 @@ package com.example.mybatisplus;
 import org.mybatis.spring.annotation.MapperScan;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.transaction.annotation.EnableTransactionManagement;
 
 @SpringBootApplication
 @MapperScan("com.example.mybatisplus.**.mapper")
+@EnableTransactionManagement
 public class MybatisPlusApplication {
 
     public static void main(String[] args) {
