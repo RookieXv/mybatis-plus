75a2cf661ea0410a3ab39e8a02e97debbd635981
liuwenxu <liuwx8102@163.com>
2021-01-06 10:43:45 +0800

    queue

---
+++ src/main/java/com/liuwenxu/data_structure/queue/Demo.java
@@ -0,0 +1,14 @@
+package com.liuwenxu.data_structure.queue;
+
+/**
+ * Copyright (C), 2016-2020, https://liuwenxu.com/
+ * FileName: Demo
+ * Author: liuwenxu
+ * Date: 2020/12/30 14:48
+ * Description: 队列
+ */
+public class Demo {
+    public static void main(String[] args) {
+
+    }
+}
